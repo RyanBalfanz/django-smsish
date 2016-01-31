149c5fb9651f8e33a5e2984f47f8de05c02c1e43
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-01-30 18:41:26 -0800

    Add 'Programming Language :: Python :: 3.4' to classifiers
    https://travis-ci.org/RyanBalfanz/django-smsish/builds/105968596

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
 		'Operating System :: OS Independent',
 		'Programming Language :: Python',
 		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
 		'Topic :: Communications',
 		'Topic :: Communications :: Telephony',
