09ef51537aee7c1e3621e16b7ddf098b38b9fb95
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-03-19 13:45:23 -0700

    Update docs

--- smsish/sms/__init__.py
+++ smsish/sms/__init__.py
@@ -19,9 +19,13 @@ __all__ = [
 
 def get_sms_connection(backend=None, fail_silently=False, **kwds):
 	"""Load an sms backend and return an instance of it.
+
 	If backend is None (default) settings.SMS_BACKEND is used.
+
 	Both fail_silently and other keyword arguments are used in the
 	constructor of the backend.
+
+	https://github.com/django/django/blob/master/django/core/mail/__init__.py#L28
 	"""
 	klass = import_string(backend or settings.SMS_BACKEND)
 	return klass(fail_silently=fail_silently, **kwds)
@@ -31,10 +35,14 @@ def send_sms(message, from_number, recipient_list, fail_silently=False, auth_use
 	"""
 	Easy wrapper for sending a single message to a recipient list. All members
 	of the recipient list will see the other recipients in the 'To' field.
+
 	If auth_user is None, the EMAIL_HOST_USER setting is used.
 	If auth_password is None, the EMAIL_HOST_PASSWORD setting is used.
+
 	Note: The API for this method is frozen. New code wanting to extend the
 	functionality should use the EmailMessage class directly.
+
+	https://github.com/django/django/blob/master/django/core/mail/__init__.py#L40
 	"""
 	connection = connection or get_sms_connection(username=auth_user, password=auth_password, fail_silently=fail_silently)
 	mail = SMSMessage(message, from_number, recipient_list, connection=connection)
