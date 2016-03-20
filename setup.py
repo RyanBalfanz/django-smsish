4d450e3d217cea9e373f16c5e4f0beb3218bd5c9
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-03-20 00:13:38 -0700

    Bump to v1.3.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name='django-smsish',
-	version='1.3.0',
+	version='1.3.2',
 	# packages=[
 	# 	'smsish',
 	# 	'smsish.mail',
